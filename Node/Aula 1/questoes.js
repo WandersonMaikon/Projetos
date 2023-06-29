edc3696106148ec4fc25584f43e6e42f863941e7
Wanderson Maikon <wandersonmaikon@gmail.com>
2023-06-29 15:03:52 -0400

    sss

---
+++ Node/Aula 1/questoes.js
@@ -0,0 +1,54 @@
+/*
+var metragem = 123; 
+var quartos = 3; 
+var preco = calcularImovel(metragem, quartos);
+console.log(`A casa custa R$: ${preco}`);
+function calcularImovel(metragem, quartos){
+    const m2 = 3000;
+    let preco = m2 * metragem;
+    switch(quartos){
+     case 1:
+        preco = preco * 1;
+        break;
+     case 2:
+        preco = preco * 1.2;
+        break;
+     case 3:
+        preco = preco * 1.5;
+        break;
+    }
+    return preco; 
+}*/
+
+/*
+function validar(usuario, senha){
+    if(usuario === 'pedro' && senha === '123'){ 
+        return true;
+    }else{
+        return false;
+    }
+}
+
+let usuario = 'pedro';
+let senha = '123';
+let validacao = validar(usuario, senha);
+if (validacao){
+    console.log("Acesso concedido!");
+}else{
+    console.log("Acesso negado");
+} */
+
+/*
+let carro = ['BMW', 'Ferrari', 'Mercedes'];
+
+console.log(carro[1]);
+
+carro[1] = "Audi";
+
+console.log(carro);
+
+carro.push('Volvo');
+console.log(carro)
+
+console.log("A quantidade de carros é de: "+carro.length);
+*/
